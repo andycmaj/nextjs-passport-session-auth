564566c9c5c620a2f171db85ff9b8f79b53d4f9e
chanced <chanceusc@gmail.com>
2020-02-08 11:47:40 -0500

    cleanup of ./lib/passport/google.js

--- lib/passport/google.ts
+++ lib/passport/google.ts
@@ -2,10 +2,12 @@ import { Strategy, Profile } from 'passport-google-oauth20'
 import appConfig from '../appConfig'
 import passport from 'passport'
 
-// STATICALLY configure the Github strategy for use by Passport.
+// STATICALLY configure the Google strategy for use by Passport.
+//
+// https://github.com/jaredhanson/passport-google-oauth2
 //
 // OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Github API on the user's
+// credential (`accessToken`) for accessing the Google API on the user's
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be exposed in the request as `req.user`
 // in api handlers after authentication.
@@ -16,19 +18,18 @@ const strategy = new Strategy({
     scope: ['profile'],
     passReqToCallback: false
   }, 
-  (accessToken, refreshToken, githubProfile, cb) => {
-    // Right now, the user's Github profile is supplied as the user
-    // record. In a production-quality application, the Github profile should
+  (accessToken, refreshToken, googleProfile, cb) => {
+    // Right now, the user's Google profile is supplied as the user
+    // record. In a production-quality application, the Google profile should
     // be associated with an app-specific user record in app persistence,
     // which allows for account linking and authentication with other identity providers.
 
     // Upsert user here
-    console.log(accessToken, refreshToken, githubProfile)
+    console.log(accessToken, refreshToken, googleProfile)
 
-    // see https://github.com/jaredhanson/passport-github/blob/master/lib/strategy.js#L40
-    // see https://gitlab.com/andycunn/canvass/blob/f3f03859b3de66f30d7703a4c5d2f44f7c724f67/api/app.js#L118
+    // see https://github.com/jaredhanson/passport-google-oauth2/blob/master/lib/strategy.js
     // for an example
-    cb(null, githubProfile)
+    cb(null, googleProfile)
   }
 )
 
@@ -39,18 +40,21 @@ const strategy = new Strategy({
 // production-quality application, this would typically be as simple as
 // supplying the user ID when serializing, and querying the user record by ID
 // from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Github profile is serialized
+// example does not have a database, the complete Google profile is serialized
 // and deserialized.
-passport.serializeUser((user: Profile, done) => {
-  const { id, displayName, username, profileUrl, photos } = user
-  done(null, { id, displayName, username, profileUrl, photos })
-})
-passport.deserializeUser(async (serializedUser, done) => {
-  if (!serializedUser) {
-    return done(new Error(`User not found: ${serializedUser}`))
-  }
 
-  done(null, serializedUser)
-})
+//TODO: move this to a centeralized point outside of google.js/github.js as this shouldn't be duplicated.
+
+// passport.serializeUser((user: Profile, done) => {
+//   const { id, displayName, username, profileUrl, photos } = user
+//   done(null, { id, displayName, username, profileUrl, photos })
+// })
+// passport.deserializeUser(async (serializedUser, done) => {
+//   if (!serializedUser) {
+//     return done(new Error(`User not found: ${serializedUser}`))
+//   }
+
+//   done(null, serializedUser)
+// })
 
 export default strategy
