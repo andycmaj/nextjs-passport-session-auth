b71334834364f53b0167826d78f29d693cf40001
chanced <chanceusc@gmail.com>
2020-03-21 11:13:56 -0400

    removing bulky comments

--- lib/passport/jwt.ts
+++ lib/passport/jwt.ts
@@ -7,31 +7,6 @@ import { ParsedUrlQuery } from "querystring";
 const { keyOpts, secretOrKey } = appConfig.getJwtConfig();
 //https://tools.ietf.org/html/rfc7519#section-4.1.3
 
-// 4.1.3.  "aud" (Audience) Claim
-
-//    The "aud" (audience) claim identifies the recipients that the JWT is
-//    intended for.  Each principal intended to process the JWT MUST
-//    identify itself with a value in the audience claim.  If the principal
-//    processing the claim does not identify itself with a value in the
-//    "aud" claim when this claim is present, then the JWT MUST be
-//    rejected.  In the general case, the "aud" value is an array of case-
-//    sensitive strings, each containing a StringOrURI value.  In the
-//    special case when the JWT has one audience, the "aud" value MAY be a
-//    single case-sensitive string containing a StringOrURI value.  The
-//    interpretation of audience values is generally application specific.
-//    Use of this claim is OPTIONAL.
-
-// 4.1.1.  "iss" (Issuer) Claim
-
-//    The "iss" (issuer) claim identifies the principal that issued the
-//    JWT.  The processing of this claim is generally application specific.
-//    The "iss" value is a case-sensitive string containing a StringOrURI
-//    value.  Use of this claim is OPTIONAL.
-
-// for some reason jwt was throwing an error when I supplied these. It may
-// be the domain of 'example.com' isn't valid or they can't match. not sure
-// i'll look into it later, I guess
-
 const jwtOptions = {
   // these were explicitly referenced so the documentation could be associated
   // within the context of the jwt strategy
